0792300926803a30e4af1157ad4af23dfac286af
Suhail <suhailcollegemail@gmail.com>
2023-04-03 14:15:22 +0530

    FINAL COMMIT

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/userSLice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
   },
 });
